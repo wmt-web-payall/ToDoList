fe88d9e8649db4457f3dd9036ac4842ef79b1155
Payal <payall@webmobtech.com>
2023-08-31 10:54:36 +0530

    todo app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDo from './ToDo';
+
+function App() {
+  return (
+    <div className="App">
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
